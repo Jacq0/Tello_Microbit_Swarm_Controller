8c372f9ffdf90c07e643ecc3514b20b87101f75b
Jacq0 <59923922+Jacq0@users.noreply.github.com>
2023-04-13 21:23:29 +0100

    Updated swarm controller to include 100ms delay between commands

--- tello_swarm_controller.py
+++ tello_swarm_controller.py
@@ -1,7 +1,13 @@
 from djitellopy import TelloSwarm
 import serial_receive as sr
+import time
 
+#if the program is running or not
 running = True
+
+#the time interval so we don't spam the drones with commands.
+lastTick = int(time.time() * 1000) #time in millis
+interval = 100 
 
 #the IP addresses of the tello drones
 ips = ["192.168.1.63", "192.168.1.64"]
@@ -26,22 +32,24 @@ def stopDrones():
         drone.set_speed(0)
 
 while running:
-    values = sr.returnInputValues()
+    if (lastTick+interval) < time():
+        values = sr.returnInputValues()
 
-    if values[5] == 1:
-        stopDrones()
-        landDrones()
-        running = False
-        
-    if values[4] == 1:
-        takeoffDrones()
+        if values[5] == 1:
+            stopDrones()
+            landDrones()
+            running = False
+            
+        if values[4] == 1:
+            takeoffDrones()
 
-    fb = values[0]
-    lr = values[1]
-    ud = values[2]
-    yaw = values[3]
+        fb = values[0]
+        lr = values[1]
+        ud = values[2]
+        yaw = values[3]
 
-    sendControls(fb, lr, ud, yaw, 5)
+        sendControls(fb, lr, ud, yaw, 5)
+        lastTick = int(time.time() * 1000)
 
 
 
